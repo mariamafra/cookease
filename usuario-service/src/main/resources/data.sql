84e0eb05aab78f47e86837d7f6fbfbbb3be8faca
maria <mariaszmafra@gmail.com>
2024-09-16 20:38:13 -0300

    Data.sql usuario

--- usuario-service/src/main/resources/data.sql
+++ usuario-service/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO ROLE (nome) VALUES ('ADMIN');
 INSERT INTO ROLE (nome) VALUES ('USER');
 INSERT INTO ROLE (nome) VALUES ('CHEFE');
+
+INSERT INTO USUARIO (id, nome, email, status, password) VALUES (1, 'João da Silva', 'joao.silva@example.com', 1, '123456');
+INSERT INTO USUARIO (id, nome, email, status, password) VALUES (2, 'Maria Oliveira', 'maria.oliveira@example.com', 0, 'senhaForte456');
