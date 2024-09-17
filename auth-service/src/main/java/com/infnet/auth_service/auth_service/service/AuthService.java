732db15342d4120dde07596c5e755b85aed7e2b2
maria <mariaszmafra@gmail.com>
2024-09-16 22:44:30 -0300

    Atualizações Auth Service

--- auth-service/src/main/java/com/infnet/auth_service/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/infnet/auth_service/auth_service/service/AuthService.java
@@ -1,13 +1,17 @@
 package com.infnet.auth_service.auth_service.service;
 
-import com.infnet.auth_service.auth_service.model.Usuario;
-import com.infnet.auth_service.auth_service.service.feign.UsuarioClient;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class AuthService {
