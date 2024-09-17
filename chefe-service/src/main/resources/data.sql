a9cc136d17d16cb73074a5fb8e4a1807d48d5533
Rafaela Curty Breves <80177533+breves-r@users.noreply.github.com>
2024-09-17 16:26:00 -0300

    Tests e Auth Swagger

---
+++ chefe-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CHEFES(nome, sobrenome) VALUES ('Duda', 'MAFRA');
+INSERT INTO CHEFES(nome, sobrenome) VALUES ('Duda', 'SOUZA');
+INSERT INTO CHEFES(nome, sobrenome) VALUES ('Rebeca', 'MAFRA');
+INSERT INTO CHEFES(nome, sobrenome) VALUES ('Debora', 'Menezes');
